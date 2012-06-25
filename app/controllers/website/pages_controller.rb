e30b8f29d794d049039f18ada37e28796a960ba8
Yerlan Kussainov <lagox.mail@gmail.com>
2012-06-25 15:36:46 +0600

    add city

--- app/controllers/website/pages_controller.rb
+++ app/controllers/website/pages_controller.rb
@@ -1,10 +1,20 @@
 # -*- encoding : utf-8 -*-
 class Website::PagesController < ApplicationController
 
+  respond_to :html
+
   layout 'website'
 
   def index
     @w_title = "Империя квартир"
+    @search = Subject.search(params[:search])
+    if params[:search]
+      @subjects = @search.page(params[:page]).per(10)
+      if @subjects.size == 0
+        flash.now[:alert] = "Ничего не найдено"
+      end
+      respond_with(@subjects)
+    end
   end
   
   def about
