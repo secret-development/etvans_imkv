ed37c7cad0d63821cd83795dc7c4732f4aeea474
Yerlan Kussainov <lagox.mail@gmail.com>
2012-06-26 17:04:16 +0600

    hot_deals

--- app/controllers/website/deals_controller.rb
+++ app/controllers/website/deals_controller.rb
@@ -5,5 +5,8 @@ class Website::DealsController < ApplicationController
   
   def index
     @w_title = "Горячие предложения"
+    @subjects = Kaminari.paginate_array(Subject.where(:active => true).last(40).reverse).page(params[:page]).per(5)
+    
+    respond_with(@subjects)
   end
 end
