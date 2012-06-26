c81ed01b6e09cb14593ebe00cee62d09f8fa09d4
Yerlan Kussainov <lagox.mail@gmail.com>
2012-06-26 17:33:29 +0600

    migrate edit website_advice

--- db/migrate/20120626112149_create_website_advices.rb
+++ db/migrate/20120626112149_create_website_advices.rb
@@ -2,7 +2,9 @@
 class CreateWebsiteAdvices < ActiveRecord::Migration
   def change
     create_table :website_advices do |t|
-
+      t.string :title
+      t.text :body
+      t.string :author
       t.timestamps
     end
   end
