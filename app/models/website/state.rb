f2f3ec13bec27bfd755e2ac83abfbc2e11051b7c
Yerlan Kussainov <lagox.mail@gmail.com>
2012-06-28 16:50:47 +0600

    advice spec model

--- app/models/website/state.rb
+++ app/models/website/state.rb
@@ -1,3 +1,24 @@
 # -*- encoding : utf-8 -*-
 class Website::State < ActiveRecord::Base
+  # validations:
+  validates :title, :presence => true
+  validates :description, :presence => true
+  validates :address, :presence => true
+  
+  def legend_value
+    if new_record?
+      "Добавление ЖК"
+    else
+      "Редактирование ЖК"
+    end
+  end
+  
+  def button_value
+    if new_record?
+      "Добавить"
+    else
+      "Редактировать"      
+    end    
+  end
+  
 end
