dd981cf4dcd4c7004f40fccfa41a7740133c92bb
Yerlan Kussainov <lagox.mail@gmail.com>
2012-06-29 16:00:52 +0600

    one part specs for states_controller

--- app/controllers/website/states_controller.rb
+++ app/controllers/website/states_controller.rb
@@ -1,3 +1,32 @@
 # -*- encoding : utf-8 -*-
 class Website::StatesController < ApplicationController
+
+  respond_to :html
+  before_filter :all_deny
+  before_filter :settings_deny
+  
+  def index
+    @states = Website::State.page(params[:page]).per(10)
+    @title = "Информация о ЖК"
+    respond_with(@states)
+  end
+  
+  def show
+    @state = Website::State.find(params[:id])
+    @title = @state.title
+    respond_with(@state)
+  end
+  
+  def new
+    @state = Website::State.new
+    @title = "Добавление информации ЖК"
+    respond_with(@state)
+  end
+  
+  def edit
+    @state = Website::State.find(params[:id])
+    @title = "Редактирвоание информации о ЖК"
+    respond_with(@state)
+  end
+  
 end
--- spec/controllers/website/states_controller_spec.rb
+++ spec/controllers/website/states_controller_spec.rb
@@ -2,5 +2,36 @@
 require 'spec_helper'
 
 describe Website::StatesController do
-
+  render_views
+  
+  before(:each) do
+    # start auth
+    @user = Factory(:user)
+    test_log_in(@user)
+    # end auth
+    
+    @state = Factory(:state)
+  end
+  
+  describe "success requests" do
+    it "get :index" do
+      get :index
+      response.should be_success
+    end
+    
+    it "get :show" do
+      get :show, :id => @state.id
+      response.should be_success
+    end
+    
+    it "get :new" do
+      get :new
+      response.should be_success
+    end
+    
+    it "get :edit" do
+      get :edit, :id => @state.id
+      response.should be_success
+    end
+  end
 end
