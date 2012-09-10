e523ea27c148d87f29a938bfe9fe8cecf4507ded
Yerlan Kussainov <lagox.mail@gmail.com>
2012-09-10 21:12:57 +0600

    add assets path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Crm::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( websitemanifest.js website.css )
+  config.assets.precompile += ['websitemanifest.js', 'website.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
